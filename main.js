315c52607eba9dd2757887195bdf64ea45a960f3
DESKTOP-T7HINV9\sonwonjae <sonwj0915@naver.com>
2021-06-04 18:55:42 +0900

    글생성 구현

--- main.js
+++ main.js
@@ -72,24 +72,29 @@ var app = http.createServer(function (request, response) {
     }
   } else if (pathname === "/create") {
     db.query(`SELECT * FROM topic`, (err, topics) => {
-      var title = "Create";
-      var list = template.list(topics);
-      var html = template.HTML(
-        title,
-        list,
-        ` <form action="/create_process" method="post">
-            <p><input type="text" name="title" placeholder="title"></p>
-            <p>
-              <textarea name="description" placeholder="description"></textarea>
-            </p>
-            <p>
-              <input type="submit">
-            </p>
-          </form>`,
-        `<a href="/create">create</a>`
-      );
-      response.writeHead(200);
-      response.end(html);
+      db.query(`SELECT * FROM author`, (err2, authors) => {
+        var title = "Create";
+        var list = template.list(topics);
+        var html = template.HTML(
+          title,
+          list,
+          ` <form action="/create_process" method="post">
+              <p><input type="text" name="title" placeholder="title"></p>
+              <p>
+                <textarea name="description" placeholder="description"></textarea>
+              </p>
+              <p>
+                ${template.authorSelect(authors)}
+              </p>
+              <p>
+                <input type="submit">
+              </p>
+            </form>`,
+          `<a href="/create">create</a>`
+        );
+        response.writeHead(200);
+        response.end(html);
+      });
     });
   } else if (pathname === "/create_process") {
     var body = "";
@@ -104,7 +109,7 @@ var app = http.createServer(function (request, response) {
       db.query(
         `INSERT INTO topic (title, description, created, author_id) 
           VALUES(?, ?, NOW(), ?)`,
-        [title, description, 1],
+        [title, description, post.author],
         (err, result) => {
           if (err) {
             throw err;
